22c97fe0fecad24368e5b24f9bda5cbc2d2e2295
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-03-13 12:08:57 +0200

    feat(components): add about-us component

---
+++ src/app/components/about-us/about-us.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-about-us',
+  imports: [
+    // RouterOutlet,RouterLink
+  ],
+  templateUrl: './about-us.html',
+  styles: ``,
+})
+export class AboutUs {}
